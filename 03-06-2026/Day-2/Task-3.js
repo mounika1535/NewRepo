0bef92b0ce2ab4b5dc1e42fbb8546f57f5160ba8
mounika <thatikonda.mounika@nybinfotech.com>
2026-06-03 18:40:14 +0530

    03-06-2026

---
+++ 03-06-2026/Day-2/Task-3.js
@@ -0,0 +1,55 @@
+// 1)Understand browser engine workflow and DOM structure
+
+// 2)Practice JavaScript integration in HTML pages
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+    alert("Button successfully clicked!");
+});
+
+// 3)Create sample programs using variables.
+let name = "Mounika";
+console.log("Hello, " + name);
+
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log("Sum =", sum);
+
+let a = 5;
+let b = 10;
+let temp = a;
+a = b;
+b = temp;
+console.log("a =", a);
+console.log("b =", b);
+
+let x = 29;
+let y = 23;
+let z = 50;
+let average = (x + y + z) / 3;
+console.log("Average =", average);
+
+const PI = 3.14159;
+let radius = 6;
+let circumference = 2 * PI * radius;
+console.log("Circumference =", circumference);
+
+let firstName = "Mounika";
+let age = 23;
+console.log(`My name is ${firstName} and I am ${age} years old.`);
+
+//4)Learn variable declaration methods and usage.
+var Name = "vineetha";
+console.log(name);
+
+let A = 100;
+B = 150;
+console.log(B);
+
+const pi = 3.14159;
+console.log(pi);
+
+let USERNAME = "Mounika";
+let AGE = 23;
+console.log(USERNAME);
+console.log(AGE);
